2c1fe2cae0f2157b8661b9b6d7e58e9b653e95ad
Tyler Riedal <triedal@mail.usf.edu>
2016-03-24 19:23:59 -0400

    built out structure for API

--- server.js
+++ server.js
@@ -11,7 +11,84 @@ router.get('/', function(req, res) {
 	res.send('API index');
 });
 
+// Vehicles ----------------------------
+router.route('/vehicles')
+  // Get all vehicles
+  .get(function(req, res) {
+
+  })
+
+  .post(function(req, res) {
+
+  });
+
+router.route('/vehicles/:id')
+  .get(function(req, res) {
+
+  })
+
+  // Update vehicle by ID
+  .put(function(req, res) {
+
+  })
+
+  .delete(function(req, res) {
+
+  });
+
+// Customers --------------------------
+router.route('/customers')
+  // Get all customers
+  .get(function(req, res) {
+
+  })
+
+  .post(function(req, res) {
+
+  });
+
+router.route('/customers/:id')
+  .get(function(req, res) {
+
+  })
+
+  // Update customer by ID
+  .put(function(req, res) {
+
+  })
+
+  .delete(function(req, res) {
+
+  });
+
+// Rental contracts ------------------
+router.route('/contracts')
+  // Get all customers
+  .get(function(req, res) {
+
+  })
+
+  .post(function(req, res) {
+
+  });
+
+router.route('/contracts/:id')
+  .get(function(req, res) {
+
+  })
+
+  // Update contract by ID
+  .put(function(req, res) {
+
+  })
+
+  .delete(function(req, res) {
+
+  });
+
+
 app.use('/api', router);
 
+// This serves the web app on the specified port (localhost:3000)
 app.listen(port);
 console.log('The magic happens on port ' + port);

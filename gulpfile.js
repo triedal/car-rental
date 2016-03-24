aaee2e4d3f0487dd4ebe6132e414030e2136f3c2
triedal <triedal@madmobile.com>
2016-03-24 17:19:10 -0400

    API started

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,7 @@ var rs = require('run-sequence');
 gulp.task('start-db', shell.task([
 	'mysql.server restart'
 ]));
+
+gulp.task('serve', shell.task([
+	'node server.js'
+]));
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 var express = require('express');
 var mysql = require('mysql');
+
+var app = express();
+var router = express.Router();
+
+var port = process.env.PORT || 3000;
+
+// API Router Stuff
+router.get('/', function(req, res) {
+	res.send('API index');
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('The magic happens on port ' + port);

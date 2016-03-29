61089ea56583b33acf7b1f488b85eff8f1e231bf
triedal <triedal@madmobile.com>
2016-03-29 10:59:33 -0400

    minor updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var dbTask = require('gulp-db');
 var rs = require('run-sequence');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
 
 gulp.task('start-db', shell.task([
 	'mysql.server restart'
@@ -10,3 +12,12 @@ gulp.task('start-db', shell.task([
 gulp.task('serve', shell.task([
 	'node server.js'
 ]));
+
+gulp.task('browserify', function() {
+	return browserify('./ui/js/app.js')
+		.bundle()
+		// Pass desired output file to vinyl-source-stream
+		.pipe(source('bundle.js'))
+		// Start piping stream to tasks
+		.pipe(gulp.dest('./build/'));
+});
---
+++ ui/js/app.js
@@ -0,0 +1,5 @@
+var _ = require('underscore');
+
+// http://tylermcginnis.com/reactjs-tutorial-pt-2-building-react-applications-with-gulp-and-browserify/
+
+// https://keyholesoftware.com/2015/05/20/boilerplate-template-for-node-js-and-backbone-using-gulp-and-browserify/

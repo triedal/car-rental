3fff1ba4c323411bcd9b66fdb231c4159d92a02c
Tyler Riedal <triedal@mail.usf.edu>
2016-04-02 18:36:24 -0400

    started reserve page

--- ui/js/components/app.js
+++ ui/js/components/app.js
@@ -1,10 +1,14 @@
 var React = require('react');
 var Navbar = require('./navbar');
+var Reserve = require('./reserve');
 
 var App = React.createClass({
   render: function() {
       return (
+        <div>
           <Navbar />
+          <Reserve />
+        </div> 
       );
   }
 });
---
+++ ui/js/components/reserve.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Reserve = React.createClass({
+  render: function () {
+    return (
+      <div id="teaser">
+      </div>
+    );
+  }
+});
+
+module.exports = Reserve;

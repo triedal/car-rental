1f73eb2f00bba3256809138c121b672c60baf969
Tyler Riedal <triedal@mail.usf.edu>
2016-04-21 09:11:49 -0400

    dynamic vehicles added

--- ui/js/components/vehicleModels.js
+++ ui/js/components/vehicleModels.js
@@ -1,7 +1,22 @@
 var React = require('react');
 
 var VehicleModel = React.createClass({
+  buildVehicleList: function() {
+    var vehicleChoices = [];
+    _(this.props.vehicles).forEach(function(vehicle, index) {
+      vehicleChoices.push(
+        <li key={index} className="" name={vehicle.make + ' ' + vehicle.model}>
+          <a>{vehicle.make + ' ' + vehicle.model}</a>
+          <span className="">&nbsp;</span>
+        </li>
+      );
+    });
+    console.log(vehicleChoices);
+    var options = _.uniqBy(vehicleChoices, 'props.name');
+    return options;
+  },
   render: function() {
+    var vehicles = this.buildVehicleList();
     return (
       <div id="vehicles" className="container-fluid max-width">
         <div className="row">
@@ -10,7 +25,15 @@ var VehicleModel = React.createClass({
               Vehicle Models - <span className="subtitle"> Our rental fleet at a glance</span>
             </h2>
             <div className="col-md-3">
-
+              <div id="vehicle-nav-container">
+                <ul className="vehicle-nav">
+                  {vehicles}
+                </ul>
+              </div>
+              <div className="vehicle-nav-control">
+                <a className="vehicle-nav-scroll" data-direction="up" href="#"><i className="fa fa-chevron-up"></i></a>
+                <a className="vehicle-nav-scroll" data-direction="down" href="#"><i className="fa fa-chevron-down"></i></a>
+              </div>
             </div>
             <div className="vehicle-data">
               <div className="col-md-6">
